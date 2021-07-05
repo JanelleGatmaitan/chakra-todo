b275db6087fda929e425d57386d3b1a3506cd920
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-07-04 22:58:30 -0700

    styled TodoList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Heading } from '@chakra-ui/react';
-import { TodoList } from './components/TodoList';
-import { AddTodo } from './components/AddTodo';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 import { VStack, IconButton } from '@chakra-ui/react';
 import { FaSun, FaMoon } from 'react-icons/fa';
+
 function App() {
   return (
     <VStack p={4}>
@@ -10,13 +11,16 @@ function App() {
       isRound='true'
       size='lg'
       alignSelf='flex-end'/>
-      <Heading mb='8'
+      <Heading
+      mb='8'
       fontWeight='extrabold'
       size='2xl'
       bgGradient='linear(to-r, pink.500, pink.300, blue.500)'
       bgClip='text'>
         Todo Application
       </Heading>
+      <TodoList/>
+      <AddTodo></AddTodo>
     </VStack>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import {
+  VStack,
+  HStack,
+  Text,
+  IconButton,
+  StackDivider,
+  Spacer
+} from '@chakra-ui/react';
+import { FaTrash } from 'react-icons/fa';
 
 function TodoList() {
+  const todos = [
+    {
+      id: 1,
+      body: 'get groceries'
+    },
+    {
+      id: 2,
+      body: 'read book'
+    }
+  ];
   return (
-    <div></div>
-  )
+  <VStack
+    divider={<StackDivider />}
+    borderColor='gray.100'
+    borderWidth='2px'
+    padding='4'
+    borderRadius='lg'
+    width='100%'
+    maxW={{base: '90vw', sm: '80vw', lg: '50vw', xl: '40vw'}}
+    alignItems='stretch'
+  >
+    {todos.map(todo => (
+      <HStack key={todo.id}>
+        <Text>{todo.body}</Text>
+        <Spacer />
+        <IconButton icon={<FaTrash />} isRound='true'/>
+      </HStack>
+    ))
+
+    }
+  </VStack>
+  );
 }
 
 export default TodoList;
